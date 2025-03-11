133748b8d67da86575b22010ddb25a532100a071
Daniel Krastev <danielkrastev01@gmail.com>
2025-03-11 15:38:55 +0200

    Lift email state and pass to comments create

--- client/src/components/comments-create/CommentsCreate.jsx
+++ client/src/components/comments-create/CommentsCreate.jsx
@@ -1,11 +1,17 @@
-export default function CommentsCreate() {
+export default function CommentsCreate({
+    email,
+}) {
+    const commentAction = (formData) => {
+        const comment = formData.get('comment');
+        console.log(email, comment);
+        
+    }
+
     return (
         <>
-            {/* Bonus */}
-            {/* Add Comment ( Only for logged-in users, which is not creators of the current game ) */}
             <article className="create-comment">
             <label>Add new comment:</label>
-                <form className="form">
+                <form className="form" action={commentAction}>
                     <textarea
                     name="comment"
                     placeholder="Comment......"
