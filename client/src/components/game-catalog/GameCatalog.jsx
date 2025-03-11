670d62389b2b004cb2ca880740c0c26c2bedbcb0
Daniel Krastev <danielkrastev01@gmail.com>
2025-03-11 14:42:48 +0200

    Show Empty games

--- client/src/components/game-catalog/GameCatalog.jsx
+++ client/src/components/game-catalog/GameCatalog.jsx
@@ -17,10 +17,12 @@ export default function GameCatalog() {
         {/* Catalogue */}
             <section id="catalog-page">
                 <h1>All Games</h1>
-
-                {games.map(game => <GameCatalogItem key={game._id} {...game} />)}
                 
-                <h3 className="no-articles">No articles yet</h3>
+                {games.length > 0 
+                    ?  games.map(game => <GameCatalogItem key={game._id} {...game} />)
+                    :  <h3 className="no-articles">No articles yet</h3>
+                }
+                
             </section>
         </>
     )
